9ad487f9bfa0e8f1a5dd32ca954b79300a7c5d48
eunipapa <54086171+eunipapa@users.noreply.github.com>
2023-04-24 14:24:32 +0900

    Add files via upload

---
+++ response.js
@@ -0,0 +1,33 @@
+const scriptName = "test_bot";
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  
+}
+
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
+
+// 네이버 날씨 정보 파싱
+function getWeatherFromNaver(param){
+}
